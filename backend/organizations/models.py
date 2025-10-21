4565c58f2b99e3eb1ef0f87dc7d9d888e0ea2fd1
Pakos <pakostasip@gmail.com>
2025-10-21 17:05:23 +0200

    Added project and tag models

--- backend/organizations/models.py
+++ backend/organizations/models.py
@@ -82,12 +82,7 @@ class Tag(models.Model):
         return self.name
 
 
-class TagRule(models.Model):
-    pass
-
-
 class Project(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     start_dte = models.DateField()
