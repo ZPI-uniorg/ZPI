2f9019253d22510e7102be352e7f6bd386ac2db5
Pakos <pakostasip@gmail.com>
2025-10-24 00:17:06 +0200

    Added id to project

--- backend/organizations/models.py
+++ backend/organizations/models.py
@@ -87,6 +87,7 @@ class TagRule(models.Model):
 
 
 class Project(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     start_dte = models.DateField()
