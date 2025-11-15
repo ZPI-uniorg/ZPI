3e2f3fff16d35788bae67f0f91b769a5e781b2c9
Pakos <pakostasip@gmail.com>
2025-11-15 15:12:33 +0100

    delete urls

--- backend/organization_calendar/urls.py
+++ backend/organization_calendar/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from .views import get_event_test, get_all_events_test, create_event_test, update_event_test, delete_event_test
 
 urlpatterns = [
-    path("events/<int:event_id>/", get_event_test, name="get_event"),
-    path("events/", get_all_events_test, name="get_all_events"),
-    path("events/create/", create_event_test, name="create_event"),
-    path("events/update/<int:event_id>/", update_event_test, name="update_event"),
-    path("events/delete/<int:event_id>/", delete_event_test, name="delete_event"),
+
 ]
